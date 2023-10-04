56060ec93ab6f438a7e3fe1e19a841a3458a38ba
ternaksapi <yusufhaikaln7@gmail.com>
2023-10-04 11:26:16 +0700

    Tugas 5 Selesai

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,13 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from main.models import Items
 
 class ItemsForm(ModelForm):
     class Meta:
         model = Items
         fields = ['name', 'amount', 'description']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['amount'].widget.attrs.update({'class': 'form-control'})
+        self.fields['description'].widget = Textarea(attrs={'class': 'form-control'})
